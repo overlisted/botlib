8607c7b67ac57f82f1ebe968866a09e8ff3e7d4a
Ilya Maximov <mail@overlisted.net>
2020-02-17 22:40:34 +0100

    a fix

--- src/main/java/net/overlisted/botlib/command/CommandsEventListener.java
+++ src/main/java/net/overlisted/botlib/command/CommandsEventListener.java
@@ -76,7 +76,7 @@ public class CommandsEventListener extends ListenerAdapter {
 
         for(Method it: this.commandTriggers) {
           boolean hasArgumentWithSpaces = it.getParameterAnnotations().length > 0
-            && it.getParameterAnnotations()[1].length > 0
+            && it.getParameterAnnotations()[1].length > 1
             && it.getParameterAnnotations()[1][0].annotationType().equals(ContainsSpaces.class);
 
           if(
